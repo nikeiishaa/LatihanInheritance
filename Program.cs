c2d55257ef3e5acae387cd0f504779855ae21fc1
nikeiishaa <kirananikeisha99@gmail.com>
2026-03-12 10:18:11 +0700

    membuat class user

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class User
+{
+    public string nama;
+    public string email;
+    public string password;
+    public string role;
+
+    public User(string nama, string email, string password, string role)
+    {
+        this.nama = nama;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    public void TampilkanInfo()
+    {
+        Console.WriteLine($"Nama {nama} | Role: {role}");
+    }
+}
