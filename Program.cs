c05c8fb35ce75192e7fb5e4b11d1d572e422eaba
nikeiishaa <kirananikeisha99@gmail.com>
2026-05-29 16:02:21 +0700

    menambahkan kode inheritance

--- Program.cs
+++ Program.cs
@@ -1,11 +1,22 @@
-﻿public class User
+﻿Siswa siswaObj = new Siswa("Nikeisha", "nikeisha@gmail.com", "oya", "siswa", "XPG1");
+Guru guruObj = new Guru("Sinta", "sinta@gmail.com", "ungke", "guru", "IPAS");
+
+siswaObj.Tampilkaninfo();
+siswaObj.SubmitTugas();
+
+Console.WriteLine();
+
+guruObj.Tampilkaninfo();
+guruObj.NilaiTugas();
+
+public class user
 {
     public string nama;
     public string email;
     public string password;
     public string role;
 
-    public User(string nama, string email, string password, string role)
+    public user(string nama, string email, string password, string role)
     {
         this.nama = nama;
         this.email = email;
@@ -13,8 +24,38 @@
         this.role = role;
     }
 
-    public void TampilkanInfo()
+    public void Tampilkaninfo()
     {
-        Console.WriteLine($"Nama {nama} | Role: {role}");
+        Console.WriteLine($"Nama: {nama} | Role: {role}");
+    }
+}
+
+class Siswa : user
+{
+    public string ClassName;
+
+    public Siswa(string nama, string email, string password, string role, string className) : base(nama, email, password, role)
+    {
+        ClassName = className;
+    }
+
+    public void SubmitTugas()
+    {
+        Console.WriteLine($"{nama} dari kelas {ClassName} sudah mengumpulkan tugas");
+    }
+}
+
+class Guru : user
+{
+    public string Mapel;
+
+    public Guru(string nama, string email, string password, string role, string mapel) : base(nama, email, password, role)
+    {
+        Mapel = mapel;
+    }
+
+    public void NilaiTugas()
+    {
+        Console.WriteLine($"Guru {nama} telah menilai semua tugas mapel {Mapel}");
     }
 }
